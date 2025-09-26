c2a0feb39ffe21cd0bd57437a61279fa8c647bac
Maheshd75 <Maheshd7569@gmail.com>
2025-09-26 15:31:47 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,7 @@ app.use('api/message',messageRouter)
 
 const PORT = process.env.PORT || 5000;
 
-//app.listen(PORT, () => {
-// console.log(`Server is running on port ${PORT}`);
-//});
+app.listen(PORT, () => {
+ console.log(`Server is running on port ${PORT}`);
+});
 
-module.exports = app;
