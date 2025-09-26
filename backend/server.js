14ddb4ed7bdb21db406fb02b465d86d27466b4fe
Maheshd75 <Maheshd7569@gmail.com>
2025-09-26 15:34:23 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use('api/message',messageRouter)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
-});
+//app.listen(PORT, () => {
+// console.log(`Server is running on port ${PORT}`);
+//});
+module.exports = app;
 
